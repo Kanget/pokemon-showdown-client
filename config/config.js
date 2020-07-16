56354ed87dc08d2fb2efa0e47cc24854c128245c
lhsackerson <drewsteracker@gmail.com>
2020-07-15 23:11:21 -0400

    v7.0.1

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (415dc34c)";
+/* version */ Config.version = "0.11.2 (f73511c4)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
--- replays/theme/wrapper.inc.php
+++ replays/theme/wrapper.inc.php
@@ -91,16 +91,16 @@ function ThemeFooterTemplate() {
 	<script>
 		soundManager.setup({url: '//play.pokemonshowdown.com/swf/'});
 	</script>
-	<script src="//play.pokemonshowdown.com/config/config.js?095c3f6f"></script>
+	<script src="//play.pokemonshowdown.com/config/config.js?af3c3116"></script>
 	<script src="//play.pokemonshowdown.com/js/battledata.js?c8038c15"></script>
 	<script src="//play.pokemonshowdown.com/data/pokedex-mini.js?d8336797"></script>
 	<script src="//play.pokemonshowdown.com/data/pokedex-mini-bw.js?2e6e73d9"></script>
 	<script src="//play.pokemonshowdown.com/data/graphics.js?1adc0939"></script>
-	<script src="//play.pokemonshowdown.com/data/pokedex.js?884bbda6"></script>
-	<script src="//play.pokemonshowdown.com/data/items.js?3ad1bc4e"></script>
+	<script src="//play.pokemonshowdown.com/data/pokedex.js?95971b62"></script>
+	<script src="//play.pokemonshowdown.com/data/items.js?2da074b0"></script>
 	<script src="//play.pokemonshowdown.com/data/moves.js?f5491a3b"></script>
-	<script src="//play.pokemonshowdown.com/data/abilities.js?344f8211"></script>
-	<script src="//play.pokemonshowdown.com/data/teambuilder-tables.js?0a5fe52d"></script>
+	<script src="//play.pokemonshowdown.com/data/abilities.js?8e6c991b"></script>
+	<script src="//play.pokemonshowdown.com/data/teambuilder-tables.js?13ff355c"></script>
 	<script src="//play.pokemonshowdown.com/js/battle-tooltips.js?2b80164d"></script>
 	<script src="//play.pokemonshowdown.com/js/battle.js?b0e91829"></script>
 	<script src="/js/replay.js?51f4e8f1"></script>
