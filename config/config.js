3331fa2deaa3ab1ec93b18376645e378d6eb9365
lhsackerson <drewsteracker@gmail.com>
2020-07-24 23:57:43 -0400

    v8.0.0

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (56354ed8)";
+/* version */ Config.version = "0.11.2 (e1278891/56354ed8)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
